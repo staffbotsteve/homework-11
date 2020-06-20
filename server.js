fc5f9b9f3ca62db71bbf6cda23ce3deb422a1ac3
Steven Swan <stevenswan@Stevens-MacBook-Pro.local>
2020-06-19 18:33:51 -0700

    created server

---
+++ server.js
@@ -0,0 +1,73 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Notes (DATA)
+// =============================================================
+var notes = [
+  {
+    routeName: "",
+    note: "",
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Displays a single note, or returns false
+app.get("/api/notes/:note", function(req, res) {
+  var chosen = req.params.note;
+
+  console.log(chosen);
+
+  for (var i = 0; i < notes.length; i++) {
+    if (chosen === notes[i].routeName) {
+      return res.json(notes[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Botes - takes in JSON input
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newNote
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newNote);
+
+  characters.push(newNote);
+
+  res.json(newNote);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
